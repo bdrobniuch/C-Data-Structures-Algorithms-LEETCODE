dc0d99c0785ca0c6a183b431431a465da439ae7a
BDrobniuch <43449396+bdrobniuch@users.noreply.github.com>
2025-03-23 09:12:17 +0100

    append

---
+++ DoublyLinkedList/02-EXERCISE-DLL-append.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *prev;
+    Node *next;
+
+    Node(int value)
+    {
+        this->value = value;
+        prev = nullptr;
+        next = nullptr;
+    }
+};
+
+class DoublyLinkedList
+{
+private:
+    Node *head;
+    Node *tail;
+    int length;
+
+public:
+    DoublyLinkedList(int value)
+    {
+        head = new Node(value);
+        tail = head;
+        length = 1;
+    }
+
+    void printList()
+    {
+        Node *temp = head;
+        while (temp)
+        {
+            cout << temp->value << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+
+    void getHead()
+    {
+        cout << "Head: " << head->value << endl;
+    }
+
+    void getTail()
+    {
+        cout << "Tail: " << tail->value << endl;
+    }
+
+    void getLength()
+    {
+        cout << "Head: " << length << endl;
+    }
+
+    void append(int value) {
+        
+
+        Node* newNode = new Node(value);
+
+        newNode->prev= tail;
+        tail->next = newNode;
+        tail= newNode;
+        length++;
+
+    }
+
+};
+
+int main () {
+    DoublyLinkedList* myDLL = new DoublyLinkedList(1);
+    myDLL->append(2);
+    myDLL->printList();
+}
