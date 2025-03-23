afdeac49f5151e2792fc149a7e232d757dcc5e87
BDrobniuch <43449396+bdrobniuch@users.noreply.github.com>
2025-03-23 09:04:37 +0100

    DLL class

---
+++ DoublyLinkedList/01-EXERCISE-DLL-class.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *prev;
+    Node *next;
+
+    Node(int value)
+    {
+        this->value = value;
+        prev = nullptr;
+        next = nullptr;
+    }
+};
+
+class DoublyLinkedList
+{
+private:
+    Node *head;
+    Node *tail;
+    int length;
+
+public:
+    DoublyLinkedList(int value)
+    {
+        head = new Node(value);
+        tail = head;
+        length = 1;
+    }
+
+    void printList()
+    {
+        Node *temp = head;
+        while (temp)
+        {
+            cout << temp->value << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+
+    void getHead()
+    {
+        cout << "Head: " << head->value << endl;
+    }
+
+    void getTail()
+    {
+        cout << "Tail: " << tail->value << endl;
+    }
+
+    void getLength()
+    {
+        cout << "Head: " << length << endl;
+    }
+
+};
+
+int main () {
+    DoublyLinkedList* myDLL = new DoublyLinkedList(1);
+
+    myDLL->printList();
+}
