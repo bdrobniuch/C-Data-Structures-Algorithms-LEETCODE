419ed364ad889f677457fac38f6125928370dfc5
BDrobniuch <43449396+bdrobniuch@users.noreply.github.com>
2025-04-05 15:56:43 +0200

    BST constructor

--- Trees/00-EXERCISE-Trees-constructor.cpp
+++ Trees/00-EXERCISE-Trees-constructor.cpp
@@ -3,12 +3,34 @@
 using namespace std;
 
 class Node {
+    public:
     int value;
     Node* left;
     Node* right;
 
-    public:
+    
     Node(int value) {
         this->value = value;
+        left = nullptr;
+        right = nullptr;
     }
+};
+
+class BinarySearchTree {
+   
+
+    public:
+    Node* root;
+
+    BinarySearchTree() {
+        root = nullptr;
+    }
+};
+
+
+int main () {
+    BinarySearchTree* newBST =  new BinarySearchTree();
+    cout << "Root " << newBST->root;
 }
+
+
