2c1191d92e622c087b98bd9346f5dc552b22cc04
BDrobniuch <43449396+bdrobniuch@users.noreply.github.com>
2025-04-05 18:18:59 +0200

    set

--- HashTables/00-EXERCISE-HashTable.cpp
+++ HashTables/00-EXERCISE-HashTable.cpp
@@ -2,51 +2,87 @@
 
 using namespace std;
 
-class Node {
-    public:
+class Node
+{
+public:
     string key;
     int value;
-    Node* next;
+    Node *next;
 
     Node(string key, int value)
     {
         this->key = key;
-        this->value= value;
-        next= nullptr;
+        this->value = value;
+        next = nullptr;
     }
 };
 
-class HashTable {
-    private:
-        static const int SIZE = 7;
-        Node* dataMap[SIZE];
-    public:
+class HashTable
+{
+private:
+    static const int SIZE = 7;
+    Node *dataMap[SIZE];
 
-    int hash(string key) {
-        int hash=0;
-        for (int i = 0; i<key.length(); i++){
+public:
+    int hash(string key)
+    {
+        int hash = 0;
+        for (int i = 0; i < key.length(); i++)
+        {
             int asciValue = int(key[i]);
-            hash= (hash+ asciValue * 23) % SIZE;
+            hash = (hash + asciValue * 23) % SIZE;
         }
         return hash;
     }
 
-    void printTable() {
-        cout<< "Table: ";
-        for (int i= 0; i< SIZE; i++)
+    void set(string key, int value)
+    {
+        int index = hash(key);
+        Node *newNode = new Node(key, value);
+
+        if (!dataMap[index])
+        {
+            dataMap[index] = newNode;
+        }
+        else
+        {
+            Node *temp = dataMap[index];
+
+            while (temp->next)
+            {
+                temp = temp->next;
+            }
+            temp->next = newNode;
+        }
+    }
+
+    void printTable()
+    {
+        cout << "Table: ";
+        for (int i = 0; i < SIZE; i++)
         {
             cout << "[" << i << "] ";
-            Node* n = dataMap[i];
-            while (n) {
-                cout<< "Key: " << n->key << " Value: " << n->value;
-                n= n->next;
+            Node *n = dataMap[i];
+            while (n)
+            {
+                cout << "Key: " << n->key << " Value: " << n->value << " ";
+                n = n->next;
             }
         }
-        cout<< endl;
+        cout << endl;
     }
 };
 
-int main() {
-    HashTable* newHT = new HashTable();
+int main()
+{
+    HashTable *newHT = new HashTable();
+    newHT->set("pies", 3);
+    newHT->set("kot", 4);
+    newHT->set("ryba", 5);
+    newHT->set("krowa", 19);
+    newHT->set("sowa", 3);
+    newHT->set("malpa", 4);
+    newHT->set("skorpion", 5);
+    newHT->set("koczkodan", 19);
     newHT->printTable();
 }
