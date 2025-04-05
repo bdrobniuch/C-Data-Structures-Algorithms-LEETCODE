2704b38ed1ad6ce2eb3d443051cfa500f6e69fe7
BDrobniuch <43449396+bdrobniuch@users.noreply.github.com>
2025-04-05 16:28:52 +0200

    insert my

--- Trees/00-EXERCISE-Trees-constructor.cpp
+++ Trees/00-EXERCISE-Trees-constructor.cpp
@@ -17,7 +17,22 @@ class Node {
 };
 
 class BinarySearchTree {
-   
+    private:
+    void insertAtNode(Node* &node, int value) {
+        if (!node) node = new Node(value); 
+        else if (node->value == value) return;
+        else if (node->value > value) {
+            insertAtNode(node->left, value);
+        } else {
+            insertAtNode(node->right, value);
+        }
+    }
+
+    void printNode(Node* node) {
+        if (node->left) printNode(node->left);
+        cout << "Node " << node->value << endl;
+        if (node->right) printNode(node->right);
+    }
 
     public:
     Node* root;
@@ -25,12 +40,30 @@ class BinarySearchTree {
     BinarySearchTree() {
         root = nullptr;
     }
+
+    void insert(int value) {
+
+            insertAtNode(root, value);
+
+    }
+
+    void printTree () {
+        printNode(root);
+    }
+
 };
 
 
 int main () {
     BinarySearchTree* newBST =  new BinarySearchTree();
-    cout << "Root " << newBST->root;
+    cout << "Root " << newBST->root << endl;
+    newBST->insert(4);
+    newBST->insert(11);
+    newBST->insert(1);
+    newBST->insert(19);
+    newBST->insert(15);
+    newBST->insert(7);
+    newBST->printTree();
 }
 
 
