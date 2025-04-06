be6ded8862069c4a11272448564f51d96b06b47b
BDrobniuch <43449396+bdrobniuch@users.noreply.github.com>
2025-04-06 11:24:24 +0200

    Graph start

---
+++ Graph/00-EXERCISE-Graphs.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+class Grap {
+    private:
+        unordered_map<string,unordered_set<string>> adjList;
+} 
