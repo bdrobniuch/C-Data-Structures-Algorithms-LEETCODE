5c26924d86032d44464ae8a548f84c16951f74aa
BDrobniuch <43449396+bdrobniuch@users.noreply.github.com>
2025-04-06 11:45:53 +0200

    insert and print

--- Graph/00-EXERCISE-Graphs.cpp
+++ Graph/00-EXERCISE-Graphs.cpp
@@ -4,7 +4,38 @@
 
 using namespace std;
 
-class Grap {
-    private:
-        unordered_map<string,unordered_set<string>> adjList;
-} 
+class Graph
+{
+private:
+    unordered_map<string, unordered_set<string>> adjList;
+
+public:
+    bool addVertex(string s)
+    {
+        if (adjList.find(s) != adjList.end())
+            return false;
+        adjList.insert(std::make_pair(s, unordered_set<std::string>()));
+        return true;
+    }
+    void print()
+    {
+        for (auto k : adjList)
+        {
+            cout << k.first << ": [";
+            for (auto s : k.second) {
+                cout << s << ", ";
+            }
+            cout << "]" <<endl;
+        }
+    }
+};
+
+int main () {
+    Graph graph;
+    graph.addVertex("A");
+    graph.addVertex("B");
+    graph.addVertex("C");
+    graph.addVertex("D");
+
+    graph.print();
+}
