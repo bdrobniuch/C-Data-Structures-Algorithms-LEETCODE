938bfeaeebeb9d7d6c22127af9250a0f9219c0bd
BDrobniuch <43449396+bdrobniuch@users.noreply.github.com>
2025-04-08 16:11:20 +0200

    selectionSort using algorythm

--- BasicSorts/selectionSort.cpp
+++ BasicSorts/selectionSort.cpp
@@ -1,7 +1,33 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int main() {
-    
+void selectionSort(vector<int> &v)
+{
+
+    for (auto i = v.begin(); i != v.end(); ++i)
+    {
+       auto minE = std::min_element(i, v.end());
+       if (i != minE) {
+            if (*i> *minE) std::swap(*i, *minE);
+       }
+
+    }
+}
+
+int main()
+{
+    vector<int> v = {4, 2, 6, 5, 1, 3};
+
+    for (auto i : v)
+        cout << i << ", ";
+    cout << endl;
+
+    selectionSort(v);
+
+    for (auto i : v)
+        cout << i << ", ";
+    cout << endl;
 }
