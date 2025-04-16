84d290c53cb170d1e037d49b397e9f9d4eff32cf
BDrobniuch <43449396+bdrobniuch@users.noreply.github.com>
2025-04-16 16:01:39 +0200

    more

--- leet/hp.cpp
+++ leet/hp.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <vector>
 #include <unordered_set>
+#include <algorithm>
+#include <string> 
+#include <numeric>
 
 using namespace std;
 
 
 
 int main () {
+
 
 
 }
